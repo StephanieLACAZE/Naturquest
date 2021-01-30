9023ec1bde9e5d1d4785a3db9b56f70275221b0a
aymone2k <73581149+aymone2k@users.noreply.github.com>
2021-01-30 13:10:53 +0100

    telechargement images result

--- src/Controller/CEntity/ResultCrudController.php
+++ src/Controller/CEntity/ResultCrudController.php
@@ -4,6 +4,14 @@ namespace App\Controller\CEntity;
 
 use App\Entity\Result;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ResultCrudController extends AbstractCrudController
 {
@@ -12,14 +20,20 @@ class ResultCrudController extends AbstractCrudController
         return Result::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IntegerField::new('id')->onlyOnIndex(),
+            
+            IdField::new('resultName'),
+            TextEditorField::new('text'),
+            $pictureFile=TextareaField::new('resultPhotoFile')->setFormType(VichImageType::class)->setLabel('Image'),
+            $pictureFile=TextareaField::new('photoSpeciesFile')->setFormType(VichImageType::class)->setLabel('Image'),
+            $pictureFile=TextareaField::new('photoMoreFile')->setFormType(VichImageType::class)->setLabel('Image'),
+            $pictureFile=TextareaField::new('photoComplementaryFile')->setFormType(VichImageType::class)->setLabel('Image'),
+         
         ];
     }
-    */
+    
 }
--- src/Entity/Result.php
+++ src/Entity/Result.php
@@ -6,9 +6,12 @@ use App\Repository\ResultRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=ResultRepository::class)
+ * @Vich\Uploadable
  */
 class Result
 {
@@ -32,24 +35,51 @@ class Result
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @var string | null
      */
     private $resultPhoto;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string | null
      */
     private $photoSpecies;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string | null
      */
     private $photoMore;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string | null
      */
     private $photoComplementary;
 
+    /**
+     * @Vich\UploadableField(mapping="result_photo", fileNameProperty="resultPhoto")
+     * @var File | null
+     */
+    private $resultPhotoFile;
+    /**
+     * @Vich\UploadableField(mapping="result_species", fileNameProperty="photoSpecies")
+     * @var File | null
+     */
+    private $photoSpeciesFile;
+    /**
+     * @Vich\UploadableField(mapping="result_more", fileNameProperty="photoMore")
+     * @var File | null
+     */
+    private $photoMoreFile;
+    /**
+     * @Vich\UploadableField(mapping="result_complementary", fileNameProperty="photoComplementary")
+     * @var File | null
+     */
+    private $photoComplementaryFile;
+   
+   
+   
     
 
     public function __construct()
@@ -87,52 +117,105 @@ class Result
         return $this;
     }
 
-    public function getResultPhoto(): ?string
+    public function setResultPhotoFile(?File $resultPhotoFile = null):void
+    {
+        $this->resultPhotoFile = $resultPhotoFile;
+        if(null!== $resultPhotoFile){
+            $this ->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getResultPhotoFile():?File
+    {
+        return $this->resultPhotoFile;
+    }
+
+    public function setResultPhoto(?string $resultPhoto):void
+    {
+        $this->resultPhoto = $resultPhoto;
+    }
+
+    public function getResultPhoto():?string
     {
         return $this->resultPhoto;
     }
 
-    public function setResultPhoto(string $resultPhoto): self
-    {
-        $this->resultPhoto = $resultPhoto;
 
-        return $this;
+    public function setPhotoSpeciesFile(?File $photoSpeciesFile = null):void
+    {
+        $this->photoSpeciesFile = $photoSpeciesFile;
+        if(null!== $photoSpeciesFile){
+            $this ->updatedAt = new \DateTime('now');
+        }
     }
 
-    public function getPhotoSpecies(): ?string
+    public function getPhotoSpeciesFile():?File
+    {
+        return $this->photoSpeciesFile;
+    }
+
+    public function setPhotoSpecies(?string $photoSpecies):void
+    {
+        $this->photoSpecies = $photoSpecies;
+    }
+
+    public function getPhotoSpecies():?string
     {
         return $this->photoSpecies;
     }
 
-    public function setPhotoSpecies(?string $photoSpecies): self
-    {
-        $this->photoSpecies = $photoSpecies;
 
-        return $this;
+    public function setPhotoMoreFile(?File $photoMoreFile = null):void
+    {
+        $this->photoMoreFile = $photoMoreFile;
+        if(null!== $photoMoreFile){
+            $this ->updatedAt = new \DateTime('now');
+        }
     }
 
-    public function getPhotoMore(): ?string
+    public function getPhotoMoreFile():?File
+    {
+        return $this->photoMoreFile;
+    }
+
+    public function setPhotoMore(?string $photoMore):void
+    {
+        $this->photoMore = $photoMore;
+    }
+
+    public function getPhotoMore():?string
     {
         return $this->photoMore;
     }
 
-    public function setPhotoMore(?string $photoMore): self
+    public function setPhotoComplementaryFile(?File $photoComplementaryFile = null):void
     {
-        $this->photoMore = $photoMore;
-
-        return $this;
+        $this->photoComplementaryFile = $photoComplementaryFile;
+        if(null!== $photoComplementaryFile){
+            $this ->updatedAt = new \DateTime('now');
+        }
     }
 
-    public function getPhotoComplementary(): ?string
+    public function getPhotoComplementaryFile():?File
+    {
+        return $this->photoComplementaryFile;
+    }
+
+    public function setPhotoComplementary(?string $photoComplementary):void
+    {
+        $this->photoComplementary = $photoComplementary;
+    }
+
+    public function getPhotoComplementary():?string
     {
         return $this->photoComplementary;
     }
 
-    public function setPhotoComplementary(?string $photoComplementary): self
-    {
-        $this->photoComplementary = $photoComplementary;
 
-        return $this;
-    }            
-            
-        }
+    public function __toString():string
+    {
+        return $this->name;
+    }
+}
+
+
